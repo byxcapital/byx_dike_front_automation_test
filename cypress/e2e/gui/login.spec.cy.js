972e497dbbb101b61d3339c45c164dcbb6dab619
Carlos Moreira <carlos.moreira@grupobyx.com.br>
2024-07-24 12:29:13 -0300

    Ajustes no teste

--- cypress/e2e/gui/login.spec.cy.js
+++ cypress/e2e/gui/login.spec.cy.js
@@ -8,7 +8,7 @@ describe('Login Screen Tests', () => {
         cy.visit('/')
     });
 
-    it.only('should successfully log in with valid credentials', () => {
+    it('should successfully log in with valid credentials', () => {
         cy.login(loginData.valid.username, loginData.valid.password);
         cy.get('.sc-dAlxHm > .ant-typography').should('be.visible').and('contain', 'Conciliation as a Service');
         cy.checkLoginSuccessMessage();
